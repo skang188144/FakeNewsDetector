8473cec7f81223c74630224a66f20898288c4503
Sanghyeok Kang <31328994+skang188144@users.noreply.github.com>
2024-07-13 17:48:11 -0400

    Start styling the main page

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ function App() {
   return (
     <div className='App'>
       <div className='SourcesColumn'>
-        <div className='SourcesColumnHeader'>
+        <div className='SourcesColumnPageTitle'>
           fake
         </div>
+
+        <div className='SourcesColumnSpacer'/>
+
+        <div className='SourcesColumnHeader'>
+          Sources
+        </div>
       </div>
+
       <div className='MainColumn'>
-        <div className='MainColumnHeader'>
+        <div className='MainColumnPageTitle'>
           news detector
         </div>
+
         <div className='MainColumnByline'>
           by Sanghyeok Kang, powered by Tavily AI
         </div>
+
+        <div className='MainColumnHeader'>
+          Query
+        </div>
+
+        <input className='MainColumnInput' placeholder='Enter a statement to fact check.'/>
       </div>
     </div>
   )
